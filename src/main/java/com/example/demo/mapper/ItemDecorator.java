c16b67f8094359ea3b2b3eb3ec71fcae628e2052
shimon gueta <shimon.gueta@gmail.com>
2020-12-15 20:43:30 +0200

    second commit

--- src/main/java/com/example/demo/mapper/ItemDecorator.java
+++ src/main/java/com/example/demo/mapper/ItemDecorator.java
@@ -30,6 +30,7 @@ public abstract  class ItemDecorator implements  ItemMapper{
         itemDto.setItemType(item.getItemType());
         itemDto.setPrice(item.getPrice());
         itemDto.setItemName(item.getName());
+
         return itemDto;
     }
 
